adc1c26a6f898a6dc47be712a8c95374884fc2a6
fcalmet <fcalmet@gmail.com>
2025-07-23 14:49:42 -0400

    Fix typo in VOCAL-Penn results section

--- hepatology.js
+++ hepatology.js
@@ -361,7 +361,7 @@ function calculateVocalPenn() {
 
   setResult(`
   <ul>
-    <li>VOCAL-Penn score}
+    <li>VOCAL-Penn score:
    <ul><li>30-day mortality risk: ${vp[0]}</li></ul>
    <ul><li>90-day mortality risk: ${vp[1]}</li></ul>
    <ul><li>180-day mortality risk: ${vp[2]}</li></ul>
